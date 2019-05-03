0634f69017a3617d41eadcf33060b23722f8e1ab
imsourceboom <ecmascript.developer@gmail.com>
2019-05-03 10:35:36 +0900

    swiper speed up

--- src/es6/swiper/index.js
+++ src/es6/swiper/index.js
@@ -3,7 +3,8 @@ import Swiper from 'swiper';
 new Swiper('.swiper-container', {
     direction: 'vertical',
     slidesPerView: 1,
-    spaceBetween: 30,
+    // spaceBetween: 30,
+    speed: 1000,
     mousewheel: true,
     pagination: {
         el: '.swiper-pagination',
