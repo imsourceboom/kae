0bb6084dd1889ebd89679bc0cfb98f5f8ce8f4fe
imsourceboom <ecmascript.developer@gmail.com>
2019-05-09 16:48:17 +0900

    about us page 95% complete and header script add

--- src/es6/elements.js
+++ src/es6/elements.js
@@ -7,6 +7,7 @@ export const body = {
 export const header = {
     self: doc.querySelector('body > header'),
     logo: doc.querySelector('body > header figure '),
+    logoImage: doc.querySelector('figure.logo img'),
     nav: doc.querySelector('body > header nav'),
     li: doc.querySelectorAll('body > header nav ul li'),
     hamburger: doc.querySelector('body > header .hamburger'),
@@ -15,6 +16,12 @@ export const header = {
 export const main = {
     self: doc.querySelector('body > main'),
     section: doc.querySelectorAll('body > main > section')
+}
+
+export const aboutUs = {
+    self: doc.querySelector('body > main > section.about-us'),
+    introduce: doc.querySelector('section.about-us > article.introduce'),
+    history: doc.querySelector('section.about-us > article.history'),
 }
 
 export const swiper = {
--- src/es6/header/index.js
+++ src/es6/header/index.js
@@ -1,18 +1,80 @@
 import {
     header,
-    swiper
+    swiper,
+    aboutUs
 } from '../elements';
 
+const about = aboutUs.self;
+const introduce = aboutUs.introduce;
+const history = aboutUs.history;
 const headerSelf = header.self;
+const hamburger = header.hamburger;
 const logo = header.logo;
+
+const logoPink = `
+    <a href="/index.html">
+        <img src="/assets/images/logo.png" alt="logo"/>
+    </a>
+`;
+const logoWhite = `
+    <a href="/index.html">
+        <img src="/assets/images/logo-white.png" alt="logo"/>
+    </a>
+`;
+
 const swiperContainer = swiper.container;
 // const swiperWrapper = swiper.wrapper;
 // const swiperSlide = Array.from(swiper.slide);
 
+
+
+/*
+    index Page
+*/
+
 if (swiperContainer !== null) {
     logo.classList.add('none');
+    logo.innerHTML = logoPink;
 }
 
 if (swiperContainer === null) {
     headerSelf.style.backgroundColor = '#f8f3e6';
+}
+
+
+
+
+/*
+    About Page
+*/
+
+if (about !== null) {
+    headerSelf.style.backgroundColor = 'rgba(255,255,255,0)';
+    headerSelf.style.color = 'rgba(255,255,255,1)';
+
+    logo.innerHTML = logoWhite;
+    hamburger.classList.add('white');
+
+    document.addEventListener('scroll', function () {
+        const historyTop = history.getBoundingClientRect().top;
+        const introduceTop = introduce.getBoundingClientRect().top;
+        console.log(introduceTop);
+
+        if (introduceTop < 0) {
+            headerSelf.style.backgroundColor = 'rgba(0,0,0,.66)';
+        } else if (introduceTop == 0 || introduceTop > 0) {
+            headerSelf.style.backgroundColor = 'rgba(0,0,0,0)';
+        }
+
+        if (historyTop < 30) {
+            headerSelf.style.backgroundColor = 'rgba(248, 243, 230, .88)';
+            headerSelf.style.color = 'rgba(255, 30, 142, 1)';
+            logo.innerHTML = logoPink;
+            hamburger.classList.remove('white');
+        } else {
+            headerSelf.style.color = 'rgba(255, 255,255,1)';
+            logo.innerHTML = logoWhite;
+            hamburger.classList.add('white');
+        }
+    })
 }
