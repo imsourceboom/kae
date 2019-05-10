c974a1d7281aec00537028ae671f1f19ffc89bd1
imsourceboom <ecmascript.developer@gmail.com>
2019-05-10 10:38:11 +0900

    header script updatet

--- src/es6/header/index.js
+++ src/es6/header/index.js
@@ -41,6 +41,8 @@ if (swiperContainer === null) {
     headerSelf.style.backgroundColor = '#f8f3e6';
 }
 
+console.log(logo);
+console.log(logo.innerHTML.value);
 
 
 
@@ -60,18 +62,18 @@ if (about !== null) {
         const introduceTop = introduce.getBoundingClientRect().top;
         console.log(introduceTop);
 
-        if (introduceTop < 0) {
+        if (introduceTop <= 0 && introduceTop >= -100) {
             headerSelf.style.backgroundColor = 'rgba(0,0,0,.66)';
-        } else if (introduceTop == 0 || introduceTop > 0) {
+        } else if (introduceTop == 0) {
             headerSelf.style.backgroundColor = 'rgba(0,0,0,0)';
         }
 
-        if (historyTop < 30) {
+        if (historyTop >= 0 && historyTop <= 40) {
             headerSelf.style.backgroundColor = 'rgba(248, 243, 230, .88)';
             headerSelf.style.color = 'rgba(255, 30, 142, 1)';
             logo.innerHTML = logoPink;
             hamburger.classList.remove('white');
-        } else {
+        } else if (historyTop >= 41 && historyTop <= 80) {
             headerSelf.style.color = 'rgba(255, 255,255,1)';
             logo.innerHTML = logoWhite;
             hamburger.classList.add('white');
